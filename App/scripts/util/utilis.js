a2e215fb19464ba2a0613a1c4dfa7b70ca2b7065
giuliobracci <giulio.brc@gmail.com>
2022-12-01 11:11:49 +0100

    Add middleware attribute class -Giulio

--- App/scripts/util/utilis.js
+++ App/scripts/util/utilis.js
@@ -1,4 +1,6 @@
 // TO REPLACE WITH DEPENDENCY ---> https://www.npmjs.com/package/uuid
+// DATE PARSING ---> https://day.js.org/ (2kb) or https://momentjs.com/docs/
+// TYPE VALIDATION ---> https://www.npmjs.com/package/type
 function getRandomId() {
     let min = Math.ceil(1);
     let max = Math.floor(500000);
